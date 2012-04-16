c066a2537f1690354bc07fa81eebd1ba34649efa
Anton Niklasson <niklasson.anton@gmail.com>
2012-04-16 18:35:11 +0200

    First commit.

---
+++ Controller.java
@@ -0,0 +1,69 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+
+public class Controller implements MouseListener, MouseMotionListener, KeyListener {
+
+	boolean keys[] = new boolean[256];
+	public int mouseX;
+	public int mouseY;
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		keys[arg0.getKeyCode()] = true;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		keys[arg0.getKeyCode()] = false;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		mouseX = arg0.getX();
+		mouseY = arg0.getY();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+			
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ GraphicsObject.java
@@ -0,0 +1,14 @@
+public class GraphicsObject {
+	
+	private double x = 0;
+	private double y = 0;
+	private double accelX = 0;
+	private double accelY = 0;
+	private double velocX = 0;
+	private double velocY = 0;
+	
+	
+	public void GraphicsObject() {
+		
+	}
+}
