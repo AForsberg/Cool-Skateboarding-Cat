4068bf131083e468772a1aaeaa9780d3c46b6186
Anton Forsberg <anton.forsberg.1@gmail.com>
2012-04-20 14:01:34 +0200

    Vectorclass now working.

--- Vector.java
+++ Vector.java
@@ -1,11 +1,52 @@
 public class Vector {
 
-	/*
-	 * Angle, length, composX, composX
-	 * 
-	 * Constructor, getters & setters
-	 * 
-	 */
-	
-	
+	private double length;
+	private double angle;
+
+	public Vector(double length, double angle) {
+		this.length = Math.abs(length);
+		this.angle = angle;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getAngle() {
+		return angle;
+	}
+
+	public void setAngle(double angle) {
+		this.angle = angle;
+	}
+
+	public double getComposX() {
+		return Math.cos(angle) * length;
+	}
+
+	public double getComposY() {
+		return Math.sin(angle) * length;
+	}
+
+	public String toString() {
+		return length + " - " + getDegrees();
+	}
+
+	public double getDegrees() {
+		return (180 * angle) / Math.PI;
+	}
+
+	public Vector addVector(Vector v) {
+		double xtot = getComposX() + v.getComposX();
+		double ytot = getComposY() + v.getComposY();
+		double length = Math.sqrt(xtot * xtot + ytot * ytot);
+
+		return new Vector(length, Math.atan(ytot / xtot));
+
+	}
+
 }
